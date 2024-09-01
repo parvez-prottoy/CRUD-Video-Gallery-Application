aa16ff1ee79baca511e908ea3ef657e4365d4618
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-09-01 10:08:09 +0600

    added server get related videos API

--- server/src/controllers/videos.controller.js
+++ server/src/controllers/videos.controller.js
@@ -1,5 +1,25 @@
 const VideoModel = require("../models/video.model");
 
+const getRelatedVideos = async (req, res) => {
+  try {
+    const { id, title } = req.params;
+    console.log(title);
+    const keywords = title.toLowerCase().split(/[\s,]+/);
+    const relatedVideos = await VideoModel.find({
+      _id: { $ne: id },
+      title: { $regex: keywords.join("|"), $options: "i" },
+    });
+    res.status(200).json({
+      status: "success",
+      data: relatedVideos,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: error?.message,
+    });
+  }
+};
 const getVideo = async (req, res) => {
   try {
     const { id } = req.params;
@@ -30,4 +50,4 @@ const getVideos = async (_req, res) => {
   }
 };
 
-module.exports = { getVideos, getVideo };
+module.exports = { getVideos, getVideo, getRelatedVideos };
--- server/src/routes/videos.route.js
+++ server/src/routes/videos.route.js
@@ -1,7 +1,12 @@
-const { getVideos, getVideo } = require("../controllers/videos.controller");
+const {
+  getVideos,
+  getVideo,
+  getRelatedVideos,
+} = require("../controllers/videos.controller");
 
 const router = require("express").Router();
 
+router.get("/:id/:title", getRelatedVideos);
 router.get("/:id", getVideo);
 router.get("/", getVideos);
 
